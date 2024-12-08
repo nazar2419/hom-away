d504d27624a4cabca6326e02c391665652358a05
Nazar <nazarx095@gmail.com>
2024-12-08 19:30:17 +0200

    categories list component

---
+++ components/home/PropertiesContainer.tsx
@@ -0,0 +1,6 @@
+function PropertiesContainer() {
+  return (
+    <div>PropertiesContainer</div>
+  )
+}
+export default PropertiesContainer
---
+++ components/home/PropertiesList.tsx
@@ -0,0 +1,6 @@
+function PropertiesList() {
+  return (
+    <div>PropertiesList</div>
+  )
+}
+export default PropertiesList
