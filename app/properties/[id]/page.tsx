2695f0df8a9cd12dbf50d792e677007c9627fdfa
Nazar <nazarx095@gmail.com>
2025-01-04 12:27:17 +0200

    review card component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -2,7 +2,7 @@ import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import BreadCrumbs from "../../../components/properties/BreadCrumbs";
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
-import ShareButton from '@/components/properties/ShareButton';
+import ShareButton from "@/components/properties/ShareButton";
 import ImageContainer from "@/components/properties/ImageContainer";
 import PropertyRating from "@/components/card/PropertyRating";
 import BookingCalendar from "@/components/properties/booking/BookingCalendar";
@@ -10,48 +10,61 @@ import PropertyDetails from "@/components/properties/PropertyDetails";
 import UserInfo from "@/components/properties/UserInfo";
 import { Separator } from "@/components/ui/separator";
 import Description from "@/components/properties/Description";
+import Amenities from "@/components/properties/Amenities";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import PropertyReviews from "@/components/reviews/PropertyReviews";
 
-async function  PropertyDetailsPage({params}:{
-    params: {id: string } }) {
-    const property = await fetchPropertyDetails(params.id);
-    if (!property) redirect ("/");
-    const {baths, bedrooms, beds, guests } = property;    
-    const details = {baths, bedrooms, beds, guests};
-    const firstName = property.profile.firstName;
-    const profileImage = property.profile.profileImage;
-    return (
-        <section>
-            <BreadCrumbs  name={property.name}/>
-            <header className="flex justify-between items-center mt-4">
-                <h1 className="text-4xl font-bold">
-                    {property.tagline}
-                </h1>
-                <div className="flex items-center gap-x-4">
-                    {/* share button */}
-                    <ShareButton name={property.name} propertyId={property.id} />
-                    <FavoriteToggleButton propertyId={property.id} />
-                </div>
-            </header>
-            <ImageContainer mainImage={property.image} name={property.name} />
-            <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
-                <div className="lg:col-span-8">
-                    <div className="flex gap-x-4 items-center">
-                        <h1 className="text-xl font-bold">
-                            {property.name}
-                        </h1>
-                        <PropertyRating inPage propertyId={property.id} />  
-                    </div>
-                    <PropertyDetails details={details} />
-                    <UserInfo profile={{ firstName, profileImage }} />
-                    <Separator className='mt-4' />
-                    <Description description={property.description} />
-                </div>
-                <div className="lg:col-span-4 flex flex-col items-center">
-                    {/* calendar */}
-                    <BookingCalendar />
-                </div>
-            </section>
-        </section>
-    )
+const DynamicMap = dynamic(
+  () => import("@/components/properties/PropertyMap"),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="h-[400px] w-full" />,
+  }
+);
+
+async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect("/");
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <ShareButton name={property.name} propertyId={property.id} />
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+          <PropertyDetails details={details} />
+          <UserInfo profile={{ firstName, profileImage }} />
+          <Separator className="mt-4" />
+          <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/* calendar */}
+          <BookingCalendar />
+        </div>
+      </section>
+      {/* after two column section */}
+      <SubmitReview propertyId={property.id} />
+      <PropertyReviews propertyId={property.id} />
+    </section>
+  );
 }
 export default PropertyDetailsPage;
--- components/properties/UserInfo.tsx
+++ components/properties/UserInfo.tsx
@@ -19,7 +19,7 @@ function UserInfo({ profile: {profileImage, firstName }} : UserInfoProps) {
         <div>
             <p>
                 Hosted by
-                <span className="font-bold">{firstName}</span>
+                <span className="font-bold"> {firstName} </span>
             </p>
             <p className="text-muted-foreground font-light">
                 Superhost &middot; 2 years hosting
