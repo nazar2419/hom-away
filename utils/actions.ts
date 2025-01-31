8813b2025d97d652e0a8ac2c5cdb7ed84d958687
Nazar <nazarx095@gmail.com>
2025-01-31 22:35:09 +0200

    admin page complete

--- utils/actions.ts
+++ utils/actions.ts
@@ -12,13 +12,19 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { uploadImage } from "./supabase";
 import { calculateTotals } from "./calculateTotals";
-
+import { formatDate } from "./format";
 const getAuthUser = async () => {
   const user = await currentUser();
   if (!user) {
     throw new Error("You must be logged in to access this route");
   }
   if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect("/");
   return user;
 };
 
@@ -615,12 +621,6 @@ export const fetchReservations = async () => {
   return reservations;
 };
 
-const getAdminUser = async () => {
-  const user = await getAuthUser();
-  if (user.id !== process.env.ADMIN_USER_ID) redirect("/");
-  return user;
-};
-
 export const fetchStats = async () => {
   await getAdminUser();
 
@@ -633,4 +633,33 @@ export const fetchStats = async () => {
     propertiesCount,
     bookingsCount,
   };
+};
+
+export const fetchChartsData = async () => {
+  await getAdminUser();
+  const date = new Date();
+  date.setMonth(date.getMonth() - 6);
+  const sixMonthsAgo = date;
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+  let bookingsPerMonth = bookings.reduce((total, current) => {
+    const date = formatDate(current.createdAt, true);
+    const existingEntry = total.find((entry) => entry.date === date);
+    if (existingEntry) {
+      existingEntry.count += 1;
+    } else {
+      total.push({ date, count: 1 });
+    }
+    return total;
+  }, [] as Array<{ date: string; count: number }>);
+  return bookingsPerMonth;
 };
